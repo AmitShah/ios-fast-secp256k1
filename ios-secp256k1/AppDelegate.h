485ea2ba07f5299d9dc47d6c32de440b0ef47a13
Amit Shah <amitshah0t7@gmail.com>
2018-05-29 17:52:36 -0500

    Initial Commit

---
+++ ios-secp256k1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-secp256k1
+//
+//  Created by Amit Shah on 2018-05-29.
+//  Copyright © 2018 Amit Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-secp256k1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios-secp256k1
+//
+//  Created by Amit Shah on 2018-05-29.
+//  Copyright © 2018 Amit Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
